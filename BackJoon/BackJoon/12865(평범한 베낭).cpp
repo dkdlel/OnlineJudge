253c7cc0c5c3d82a590e18a8a1975a8f44df4272
JeJn <leejj1082@icloud.com>
2021-02-05 15:27:11 +0900

    #2021/02/05

--- BackJoon/BackJoon/12865(평범한 베낭).cpp
+++ BackJoon/BackJoon/12865(평범한 베낭).cpp
@@ -13,8 +13,22 @@ using namespace std;
 
 ifstream fcin("/Users/jejn/Desktop/OnlineJudge/input.txt");
 
+int arr[101][2], cache[101][100001];
 
 int main(){
+    
+    int N, K; fcin >> N >> K;
+    for(int i = 1 ; i <= N ; i++) fcin >> arr[i][0] >> arr[i][1];
+    
+    for(int i = 1 ; i <= N ; i++){
+        int weight = arr[i][0], value = arr[i][1];
+        for(int j = 1 ; j <= K ; j++)
+            cache[i][j] = weight <= j ? max(cache[i-1][j], value + cache[i-1][j-weight]) : cache[i-1][j];
+        // 현재 무게를 넣을수 있으면 물건을 넣지않을때(i-1)과 물건을 넣을때(value + [i-1][j-weight])의 최대값 => 넣을때는 현재 무게를 빼고 계산
+        // 현재 무게를 넣을수 없으면 직전의 무게
+    }
+    
+    cout << cache[N][K] << '\n';
     
     return 0;
 }
--- BackJoon/BackJoon/1912(연속합).cpp
+++ BackJoon/BackJoon/1912(연속합).cpp
@@ -13,20 +13,47 @@ using namespace std;
 
 ifstream fcin("/Users/jejn/Desktop/OnlineJudge/input.txt");
 
+int arr[MAX], cache[MAX];
+
+int findMax(int t, int ans){
+    if(!ans){
+        ans = -987654321;
+        for(int i = 0 ; i < t ; i++)
+            if(ans < arr[i])
+                ans = arr[i];
+    }
+    return ans;
+}
+
 int main(){
     
-    int arr[MAX], cache[MAX];
+//// sol2
     int t; fcin >> t;
     for(int i = 0 ; i < t ; i++) fcin >> arr[i];
     
-    cache[0] = arr[0];
-    int ans = arr[0];
-    
-    for(int i = 1 ; i < t ; i++){
-        cache[i] = cache[i-1] + arr[i] > arr[i] ? cache[i-1] + arr[i] : arr[i];
-        ans = ans > cache[i] ? ans : cache[i];
+    int sum = 0, ans = 0;
+    for(int i = 0 ; i < t ; i++){
+        sum += arr[i];
+        if(sum <= 0) cache[i] = sum = 0;
+        else cache[i] = sum;
+        if(ans < cache[i]) ans = cache[i];
     }
     
+    cout << findMax(t, ans) << '\n';
+    
+    //// sol1
+    //    int t; fcin >> t;
+    //    for(int i = 0 ; i < t ; i++) fcin >> arr[i];
+    //
+    //    cache[0] = arr[0];
+    //    int ans = arr[0];
+    //
+    //    for(int i = 1 ; i < t ; i++){
+    //        cache[i] = cache[i-1] + arr[i] > arr[i] ? cache[i-1] + arr[i] : arr[i];
+    //        ans = ans > cache[i] ? ans : cache[i];
+    //    }
+    
+//// overtime
 //    for(int i = 0 ; i < t ; i++){
 //            int max_sum = -987654321, sum = 0;
 //            for(int j = i ; j < t ; j++){
@@ -36,7 +63,7 @@ int main(){
 //            ans = max(ans, max_sum);
 //        }
     
-    cout << ans << '\n';
+//    cout << ans << '\n';
     
     return 0;
 }
